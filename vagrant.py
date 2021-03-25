fddfdc54a756486f01beb053f68d75a395ed597d
walpurgisnatch <Kawemirs@gmail.com>
2021-03-26 06:03:13 +0700

    new flags for output

--- vagrant.py
+++ vagrant.py
@@ -11,15 +11,21 @@ from queue import Queue
 
 interesting_domains = []
 to_discover = []
+output = []
+both = False
 queue = Queue()
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "--domains", dest="domains", help="Domains to walk through")    
+    parser.add_argument("-d", "--domains", dest="domains", help="Domains to walk through")
+    parser.add_argument("-o", "--output", dest="output", nargs='*', help="Send output to file")
+    parser.add_argument("-b", "--both", dest="both", action="store_true", help="Creates two files in current directory with default names")
     arguments = parser.parse_args()
     if not arguments.domains:
         parser.error("null target")
     return arguments
+
+args = get_args()
 
 def threader():
     while True:
@@ -52,11 +58,17 @@ def check_domain(url):
         location = re.search('(.*:\/\/.*?[.].*?)\/', response.url + '/').group(1)
         if response.status_code == 200 and len(response.content) > 200 and location not in interesting_domains:              
             interesting_domains.append(location)
-            fileworks.write_line("interesting_domains.list", location)
+            if len(output) > 0 and output[0]:
+                fileworks.write_line(output[0], location)
+            elif args.both:
+                fileworks.write_line("interesting_domains.list", location)
             print("[+] {} looks interesting".format(location))
         elif response.status_code != 404 and location not in to_discover:
             to_discover.append(location)
-            fileworks.write_line("to_discover.list", location)
+            if len(output) > 1 and output[1]:
+                fileworks.write_line(output[1], location)
+            elif args.both:
+                fileworks.write_line("to_discover.list", location)            
     except Exception as e:
         pass
 
@@ -67,8 +79,14 @@ def print_results():
     print("[To discover] {}".format(len(to_discover)))
 
 def main():
-    args = get_args()
     try:
+        if args.output:
+            if len(args.output) > 0 and args.output[0]:
+                output.append(args.output[0])
+            if len(args.output) > 1 and args.output[1]:
+                output.append(args.output[1])
+        if args.both:
+            both = True
         walk_through(args.domains)
         print_results()
     except KeyboardInterrupt:
